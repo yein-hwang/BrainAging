3f94f17ddb9b872ac1308c19be1b0901e65e9291
yein-hwang <yein.y.hwang@snu.ac.kr>
2023-07-21 17:10:24 +0900

    remove file

--- shell/cust_8.sh
+++ shell/cust_8.sh
@@ -14,5 +14,5 @@ source /home/username/.bashrc
 
 echo "CustomCosineAnnealingWarmUpRestarts 적용, batch size: 8"
 
-python main_lr.py --batch_size 8 --epochs 40 --output './cust_' --ensemble_number 8 --lr 1e-6
+python main_lr.py --batch_size 8 --epochs 40 --output '.model/cust_' --ensemble_number 8 --lr 1e-6
 
