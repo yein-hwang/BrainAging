9cfe1221c66a624343896d333d76b8393d442a47
yein-hwang <yein.y.hwang@snu.ac.kr>
2023-07-21 23:57:10 +0900

    update

--- shell/cust_32.sh
+++ shell/cust_32.sh
@@ -7,12 +7,11 @@
 #SBATCH --time=0-12:00:00
 #SBATCH --nodelist=b04
 #SBATCH --mem=24000MB
-#SBATCH --cpus-per-task=32
 #SBATCH -o ./shell/cust_32_1.txt
 
 source /home/username/.bashrc
 
-echo "CustomCosineAnnealingWarmUpRestarts 적용, batch size: 32, n_workers: 16"
+echo "CustomCosineAnnealingWarmUpRestarts 적용, batch size: 32, n_workers: 8"
 
-python main_lr.py --batch_size 32 --n_workers 4 --epochs 40 --output 'model/cust_' --ensemble_number 32 --lr 1e-6
+python main_lr.py --batch_size 32 --n_workers 8 --epochs 40 --output 'model/cust_' --ensemble_number 32 --lr 1e-6
 
--- shell/cust_32_test.sh
+++ shell/cust_32_test.sh
@@ -5,14 +5,13 @@
 #SBATCH --gres=gpu:4
 #SBATCH --partition=titan
 #SBATCH --time=0-12:00:00
-#SBATCH --nodelist=a07
+#SBATCH --nodelist=a04
 #SBATCH --mem=24000MB
-#SBATCH --cpus-per-task=32
-#SBATCH -o ./shell/cust_32_test_32.txt
+#SBATCH -o ./shell/cust_32_test_0.txt
 
 source /home/username/.bashrc
 
-echo "CustomCosineAnnealingWarmUpRestarts 적용, batch size: 32, n_workers 32"
+echo "CustomCosineAnnealingWarmUpRestarts 적용, batch size: 32, n_workers 4"
 
-python main_lr.py --batch_size 32 --n_workers 8 --epochs 40 --output 'model/cust_test' --ensemble_number 32 --lr 1e-6
+python main_lr.py --batch_size 32 --n_workers 0 --epochs 40 --output 'model/cust_test' --ensemble_number 32 --lr 1e-6
 
