88cc6c9caef7e78db73be33788bb7fc7d7bdb91a
yein-hwang <yein.h.hwang@snu.ac.kr>
2023-07-28 01:31:57 +0900

    update

---
+++ shell/esb1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name=esb1
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:4
+#SBATCH --partition=titan
+#SBATCH --time=0-12:00:00
+#SBATCH --nodelist=a06
+#SBATCH --mem=24000MB
+#SBATCH -o ./shell/esb1_1.txt
+
+echo "esb_1 with four gpus"
+python main_cv.py --batch_size 32 --n_workers 8 --epochs 40 --output 'model/esb_' --ensemble_number 1 --lr 1e-6
---
+++ shell/esb2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name=esb2
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:4
+#SBATCH --partition=titan
+#SBATCH --time=0-12:00:00
+#SBATCH --nodelist=a09
+#SBATCH --mem=24000MB
+#SBATCH -o ./shell/esb2_4.txt
+
+echo "esb_2 with four gpus"
+python main_cv.py --batch_size 32 --n_workers 8 --epochs 40 --output 'model/esb_' --ensemble_number 2 --lr 1e-6
