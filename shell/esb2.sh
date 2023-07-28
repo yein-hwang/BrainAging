3dcb8b0740aa5b4a1326cd4068173eca0d807d4f
yein-hwang <yein.h.hwang@snu.ac.kr>
2023-07-28 17:42:13 +0900

    udpate

--- shell/esb2.sh
+++ shell/esb2.sh
@@ -7,7 +7,7 @@
 #SBATCH --time=0-12:00:00
 #SBATCH --nodelist=a09
 #SBATCH --mem=24000MB
-#SBATCH -o ./shell/esb2_4.txt
+#SBATCH -o ./shell/esb2_6.txt
 
 echo "esb_2 with four gpus"
 python main_cv.py --batch_size 32 --n_workers 8 --epochs 40 --output 'model/esb_' --ensemble_number 2 --lr 1e-6
