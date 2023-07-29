04cfd6a53e4fa289f1c1c13f0e83768af0164a8f
yein-hwang <yein.h.hwang@snu.ac.kr>
2023-07-29 21:22:48 +0900

    update

--- shell/esb3.sh
+++ shell/esb3.sh
@@ -7,7 +7,7 @@
 #SBATCH --time=0-12:00:00
 #SBATCH --nodelist=a04
 #SBATCH --mem=24000MB
-#SBATCH -o ./shell/esb3_6.txt
+#SBATCH -o ./shell/esb3_7.txt
 
 echo "esb_3 with four gpus"
 python main_cv.py --batch_size 32 --n_workers 8 --epochs 25 --output 'model/esb_' --ensemble_number 3 --lr 1e-6
