75d17b0d19cd81590bc8058d3c51753216712606
yein-hwang <yein.h.hwang@snu.ac.kr>
2023-07-30 22:44:44 +0900

    update

--- shell/esb1.sh
+++ shell/esb1.sh
@@ -5,9 +5,9 @@
 #SBATCH --gres=gpu:4
 #SBATCH --partition=titan
 #SBATCH --nodelist=a07
-#SBATCH --time=0-11:00:00
+#SBATCH --time=0-10:30:00
 #SBATCH --mem=24000MB
-#SBATCH -o ./shell/esb1_6.txt
+#SBATCH -o ./shell/esb1_8.txt
 
 echo "esb_1 with four gpus"
 python main_cv.py --batch_size 32 --n_workers 8 --epochs 30 --output 'model/esb_' --ensemble_number 1 --lr 1e-6
