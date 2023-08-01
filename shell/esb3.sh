02ab590fc5b03d9be7fbf0c4e9e835fd9e08b8a1
yein-hwang <yein.h.hwang@snu.ac.kr>
2023-08-01 10:26:25 -0400

    update

--- shell/esb3.sh
+++ shell/esb3.sh
@@ -4,10 +4,10 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:4
 #SBATCH --partition=titan
-#SBATCH --time=0-9:30:00
+#SBATCH --time=0-11:00:00
 #SBATCH --nodelist=a04
 #SBATCH --mem=24000MB
-#SBATCH -o ./shell/esb3_11.txt
+#SBATCH -o ./shell/esb3_14.txt
 
 echo "esb_3 with four gpus"
-python main_cv.py --batch_size 32 --n_workers 8 --epochs 30 --output 'model/esb_' --ensemble_number 3 --lr 1e-6
+python main_cv.py --batch_size 32 --n_workers 8 --epochs 40 --output 'model/esb_' --ensemble_number 3 --lr 1e-6
