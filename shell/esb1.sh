7487292f02f2aa72f669cc91e8ce9a90e5dd1d7f
yein-hwang <yein.h.hwang@snu.ac.kr>
2023-08-01 21:49:55 -0400

    update

--- shell/esb1.sh
+++ shell/esb1.sh
@@ -5,9 +5,9 @@
 #SBATCH --gres=gpu:4
 #SBATCH --partition=titan
 #SBATCH --nodelist=a07
-#SBATCH --time=0-12:00:00
+#SBATCH --time=0-11:00:00
 #SBATCH --mem=24000MB
-#SBATCH -o ./shell/esb1_11.txt
+#SBATCH -o ./shell/esb1_12.txt
 
 echo "esb_1 with four gpus"
 python main_cv.py --batch_size 32 --n_workers 8 --epochs 40 --output 'model/esb_' --ensemble_number 1 --lr 1e-6
--- shell/esb2.sh
+++ shell/esb2.sh
@@ -4,10 +4,10 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:4
 #SBATCH --partition=titan
-#SBATCH --time=0-12:00:00
+#SBATCH --time=0-11:00:00
 #SBATCH --nodelist=a09
 #SBATCH --mem=24000MB
-#SBATCH -o ./shell/esb2_16.txt
+#SBATCH -o ./shell/esb2_cv2_3.txt
 
 echo "esb_2 with four gpus"
 python main_cv.py --batch_size 32 --n_workers 8 --epochs 40 --output 'model/esb_' --ensemble_number 2 --lr 1e-6
